63e45aae80bcad1f973c0861fc1a41f4427a10e9
Sergey Vdovareize <sergey.vdovareize@inspera.no>
2022-08-31 18:17:05 +0300

    parsed data

--- scripts/mapParser.js
+++ scripts/mapParser.js
@@ -1,27 +1,52 @@
 import fetch from 'node-fetch';
 
+const getLayer = (data) => {
+  return data[1][6][0][12][0][13][0]
+}
+
+const getPolygonName = data => data[5][0]?.[1][0] || null
+
+const getPolygonDescription = data => data[5][1]?.[1][0] || null
+
+const getPolygonPoints = (data) => {
+  return data[3][0][0][0][0].map(point => {
+    return { lat: point[0][0], lng: point[0][1] } 
+  })
+}
+
+const getPolygon = (data) => {
+  const polygon = {
+    id: data[0],
+    name: getPolygonName(data),
+    description: getPolygonDescription(data),
+    points: getPolygonPoints(data),
+  }
+
+  return polygon
+}
+
+const composePolygons = (layer) => {
+  return layer.map(getPolygon)
+}
+
 const getMapData = async (mapId) => {
   const mapDataReg = /var _pageData\s*=\s*['|"](\[.*\])['|"];/igm
   const url = `https://www.google.com/maps/d/u/0/viewer?mid=${mapId}`
   console.log('map url', url)
 
   try {
-  const response = await fetch(url)
-  console.log('fetch statusCode:', response.status);
-  const body = await response.text()
-    // console.log('headers:', res.headers);
-    // console.log('res:', res);
-    // response.text().then
+    const response = await fetch(url)
+    console.log('fetch statusCode:', response.status);
+    const body = await response.text()
     const match = mapDataReg.exec(body)
     const pageData = match[1].replaceAll('\\"', '"')
     const mapData = JSON.parse(pageData)
-    // console.log(mapData)
-    // console.log(match[1].length)
+    const layer = getLayer(mapData)
+    const polygons = composePolygons(layer)
+    console.log(polygons[0])
 
-    // res.on('data', (d) => {
-    //   process.stdout.write(d);
-    // });
   } catch (error) {
+    // TODO: updat catcher
     console.error(`Couldn't fetch map. Check that map id is correct.`, error)
   }
 
