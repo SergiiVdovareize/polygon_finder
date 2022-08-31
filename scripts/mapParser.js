5f45f8dca2903c1a82f1d78d816863f8f431f08a
Sergey Vdovareize <sergey.vdovareize@inspera.no>
2022-08-31 11:26:05 +0300

    json.parse

--- scripts/mapParser.js
+++ scripts/mapParser.js
@@ -1,26 +1,35 @@
-// const https = require('node:https');
 import fetch from 'node-fetch';
 
-const getMapContent = (mapId) => {
+const getMapData = async (mapId) => {
+  const mapDataReg = /var _pageData\s*=\s*['|"](\[.*\])['|"];/igm
   const url = `https://www.google.com/maps/d/u/0/viewer?mid=${mapId}`
   console.log('map url', url)
 
-  fetch(url).then((res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-    console.log('res:', res);
+  try {
+  const response = await fetch(url)
+  console.log('fetch statusCode:', response.status);
+  const body = await response.text()
+    // console.log('headers:', res.headers);
+    // console.log('res:', res);
+    // response.text().then
+    const match = mapDataReg.exec(body)
+    const pageData = match[1].replaceAll('\\"', '"')
+    const mapData = JSON.parse(pageData)
+    // console.log(mapData)
+    // console.log(match[1].length)
 
     // res.on('data', (d) => {
     //   process.stdout.write(d);
     // });
+  } catch (error) {
+    console.error(`Couldn't fetch map. Check that map id is correct.`, error)
+  }
 
-  }).catch((error) => {
-    console.error(error);
-  });
 }
 
+
 const parse = (mapId) => {
-    getMapContent(mapId)
+    getMapData(mapId)
 }
 
 export default { parse }
