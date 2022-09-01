9fce81d942c5a6ac2d901c3e837de65e6683face
Sergey Vdovareize <sergey.vdovareize@inspera.no>
2022-09-01 18:27:55 +0300

    added checker

--- scripts/mapParser.js
+++ scripts/mapParser.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch';
 
-const getLayer = data => data[1][6]?.[0][12]?.[0][13]?.[0] || null
+const getLayer = data => data[1][6]?.[0][12]?.[0][13]?.[0] || null;
 
-const getPolygonName = data => data[5][0]?.[1][0] || null
+const getPolygonName = data => data[5][0]?.[1][0] || null;
 
-const getPolygonDescription = data => data[5][1]?.[1][0] || null
+const getPolygonDescription = data => data[5][1]?.[1][0] || null;
 
-const getPolygonPoints = data => data[3][0]?.[0]?.[0]?.[0].map(pointsArray => pointsArray[0]) || []
+const getPolygonPoints = data => data[3][0]?.[0]?.[0]?.[0].map(pointsArray => pointsArray[0]) || [];
 
 const getPolygon = (data) => {
   const polygon = {
@@ -14,47 +14,46 @@ const getPolygon = (data) => {
     name: getPolygonName(data),
     description: getPolygonDescription(data),
     points: getPolygonPoints(data),
-  }
+  };
 
-  return polygon
-}
+  return polygon;
+};
 
 const composePolygons = (layer) => {
-  return layer.map(getPolygon)
-}
+  return layer.map(getPolygon);
+};
 
 const getMapData = async (mapId) => {
-  const url = `https://www.google.com/maps/d/u/0/viewer?mid=${mapId}`
+  const url = `https://www.google.com/maps/d/u/0/viewer?mid=${mapId}`;
 
   try {
-    const response = await fetch(url)
-    console.log('fetch statusCode:', response.status);
-    const body = await response.text()
-    return body
+    const response = await fetch(url);
+    const body = await response.text();
+    return body;
   } catch (error) {
-    console.error(`Couldn't fetch map. Check that map id is correct.`, error)
-    return null
+    console.error('Couldn\'t fetch map. Check that map id is correct.', error);
+    return null;
   }
-}
+};
 
 const parseMapData = (data) => {
-  const mapDataReg = /var _pageData\s*=\s*['|"](\[.*\])['|"];/igm
+  const mapDataReg = /var _pageData\s*=\s*['|"](\[.*\])['|"];/igm;
 
   try {
-    const match = mapDataReg.exec(data)
-    const pageData = match[1].replaceAll('\\"', '"')
-    const mapData = JSON.parse(pageData)
-    const layer = getLayer(mapData)
-    return layer ? composePolygons(layer) : []
+    const match = mapDataReg.exec(data);
+    const pageData = match[1].replaceAll('\\"', '"');
+    const mapData = JSON.parse(pageData);
+    const layer = getLayer(mapData);
+    return layer ? composePolygons(layer) : [];
   } catch (error) {
-    console.error(`Couldn't parse map data.`, error)
-    return []
+    console.error('Couldn\'t parse map data.', error);
+    return [];
   }
-}
+};
 
 const parse = async (mapId) => {
-    const mapData = await getMapData(mapId)
-    return mapData ? parseMapData(mapData) : []
-}
+  const mapData = await getMapData(mapId);
+  return mapData ? parseMapData(mapData) : [];
+};
 
-export default { parse }
+export default { parse };
--- scripts/pointChecker.js
+++ scripts/pointChecker.js
@@ -1,23 +1,68 @@
-const isInside = (point, polygon) => {
-  const x = point[0]
-  const y = point[1];
-    
-  let inside = false;
+import inside from 'point-in-polygon-hao';
+import mapParser from './mapParser.js';
 
-  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-      var xi = vs[i][0], yi = vs[i][1];
-      var xj = vs[j][0], yj = vs[j][1];
-      
-      var intersect = ((yi > y) != (yj > y))
-          && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-      if (intersect) inside = !inside;
+const parseParams = () => {
+  const args = process.argv.slice(2);
+  const paramsArray = args.filter(arg => arg.indexOf('=') !== -1);
+
+  const parsedParams = {};
+  paramsArray.forEach(paramString => {
+    const keyValue = paramString.split('=');
+    parsedParams[keyValue[0]] = keyValue[1];
+  });
+
+  return parsedParams;
+};
+
+const validateParams = (params) => {
+  if (!params.map) {
+    console.log('Missed "map" parameter. It should be a map id value');
+    return false;
   }
 
-  polygon.forEach(apex => {
-    
-  })
-  
-  return inside;
-}
+  if (!params.position) {
+    console.log('Missed "position" parameter. It should contain lat and lng coordinates');
+    return false;
+  }
 
-export default { parse }
+  if (params.position.split(',').length !== 2) {
+    console.log('Wrong "position" value. It should look something like this: 45.306286,36.508117');
+    return false;
+  }
+
+  return true;
+};
+
+const parsePointCoordinates = point => point.split(',').map(coord => parseFloat(coord));
+
+const addRepeatedPoints = (polygons) => {
+  polygons
+    .filter(polygon => polygon.points.length > 2)
+    .filter(polygon => {
+      const startPoint = polygon.points[0];
+      const endPoint = polygon.points[polygon.points.length - 1];
+      return startPoint[0] !== endPoint[0] || startPoint[1] !== endPoint[1];
+    })
+    .forEach(polygon => {
+      polygon.points.push(polygon.points[0]);
+    });
+};
+
+const checkPoint = (point, polygons) => {
+  return polygons.filter(polygon => inside(point, [polygon.points]));
+};
+
+const check = async () => {
+  const parsedParams = parseParams();
+  if (!validateParams(parsedParams)) {
+    process.exit(1);
+  }
+  const polygons = await mapParser.parse(parsedParams.map);
+  addRepeatedPoints(polygons);
+
+  const point = parsePointCoordinates(parsedParams.position);
+  const relatedToPolygons = checkPoint(point, polygons);
+  return relatedToPolygons;
+};
+
+export default { check };
